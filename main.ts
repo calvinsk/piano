e60f8bd81d9a9e84b59cb854205f8a10e05fd273
calvinsk <65216949+calvinsk@users.noreply.github.com>
2020-05-18 17:53:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,10 @@
-export enum enColor {
-        //% blockId="OFF" block="OFF"
-        OFF = 0,
-        //% blockId="RED" block="RED"
-        RED,
-        //% blockId="GREEN" block="GREEN"
-        GREEN,
-        //% blockId="BLUE" block="BLUE"
-        BLUE,
-        //% blockId="WHITE" block="WHITE"
-        WHITE,
-        //% blockId="CYAN" block="CYAN"
-        CYAN,
-        //% blockId="PINKISH" block="PINKISH"
-        PINKISH,
-        //% blockId="YELLOW" block="YELLOW"
-        YELLOW
-    }
-    
+namespace piano {
 
-export enum Musictone {
+  
+    let yahStrip: neopixel.Strip;
+    
+    
+    export enum enMusic {
 
         dadadum = 0,
         entertainer,
@@ -79,7 +65,24 @@ export enum Musictone {
        
     }
     
-    
+    export enum enColor {
+        //% blockId="OFF" block="OFF"
+        OFF = 0,
+        //% blockId="RED" block="RED"
+        RED,
+        //% blockId="GREEN" block="GREEN"
+        GREEN,
+        //% blockId="BLUE" block="BLUE"
+        BLUE,
+        //% blockId="WHITE" block="WHITE"
+        WHITE,
+        //% blockId="CYAN" block="CYAN"
+        CYAN,
+        //% blockId="PINKISH" block="PINKISH"
+        PINKISH,
+        //% blockId="YELLOW" block="YELLOW"
+        YELLOW
+    }
     
 
     function i2cwrite(addr: number, reg: number, value: number) { 
@@ -88,15 +91,6 @@ export enum Musictone {
         buf[1] = value; 
         pins.i2cWriteBuffer(addr, buf); 
     }  
-
-
-namespace piano {
-
-  
-    let yahStrip: neopixel.Strip;
-    
-    
-   
          
         
    
@@ -134,28 +128,28 @@ namespace piano {
     //% blockGap=10
     //% color="#17ecc1"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function Music_Handle(index: Musictone): void {
+    export function Music_Handle(index: enMusic): void {
         switch (index) {
-            case Musictone.dadadum: music.beginMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once); break;
-            case Musictone.birthday: music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once); break;
-            case Musictone.entertainer: music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once); break;
-            case Musictone.prelude: music.beginMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once); break;
-            case Musictone.ode: music.beginMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once); break;
-            case Musictone.nyan: music.beginMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once); break;
-            case Musictone.ringtone: music.beginMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once); break;
-            case Musictone.funk: music.beginMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once); break;
-            case Musictone.blues: music.beginMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once); break;
-            case Musictone.wedding: music.beginMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once); break;
-            case Musictone.funereal: music.beginMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once); break;
-            case Musictone.punchline: music.beginMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Once); break;
-            case Musictone.baddy: music.beginMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once); break;
-            case Musictone.chase: music.beginMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once); break;
-            case Musictone.ba_ding: music.beginMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once); break;
-            case Musictone.wawawawaa: music.beginMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once); break;
-            case Musictone.jump_up: music.beginMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once); break;
-            case Musictone.jump_down: music.beginMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once); break;
-            case Musictone.power_up: music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once); break;
-            case Musictone.power_down: music.beginMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once); break;
+            case enMusic.dadadum: music.beginMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once); break;
+            case enMusic.birthday: music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once); break;
+            case enMusic.entertainer: music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once); break;
+            case enMusic.prelude: music.beginMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once); break;
+            case enMusic.ode: music.beginMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once); break;
+            case enMusic.nyan: music.beginMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once); break;
+            case enMusic.ringtone: music.beginMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once); break;
+            case enMusic.funk: music.beginMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once); break;
+            case enMusic.blues: music.beginMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once); break;
+            case enMusic.wedding: music.beginMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once); break;
+            case enMusic.funereal: music.beginMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once); break;
+            case enMusic.punchline: music.beginMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Once); break;
+            case enMusic.baddy: music.beginMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once); break;
+            case enMusic.chase: music.beginMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once); break;
+            case enMusic.ba_ding: music.beginMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once); break;
+            case enMusic.wawawawaa: music.beginMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once); break;
+            case enMusic.jump_up: music.beginMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once); break;
+            case enMusic.jump_down: music.beginMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once); break;
+            case enMusic.power_up: music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once); break;
+            case enMusic.power_down: music.beginMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once); break;
         }
     }
     
